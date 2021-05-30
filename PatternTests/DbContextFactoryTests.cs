70d24e51780f5e000b0f12e6001e849ff471ef26
shangaro <dibasbaral007@gmail.com>
2021-05-30 15:34:25 -0400

    Add Unit tests for patterns - factory pattern -builder pattern

---
+++ PatternTests/DbContextFactoryTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using FactoryPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using FactoryPattern.DbContexts;
+
+namespace PatternTests
+{
+    public class DbContextFactoryTests
+    {
+        private readonly Action<IDbContextOptionBuilder> action;
+        private readonly DbContextFactory sut;
+
+        public DbContextFactoryTests()
+        {
+            action = new Mock<Action<IDbContextOptionBuilder>>().Object;
+            sut = new DbContextFactory();
+        }
+
+        [Fact()]
+        public void CreateGraphTest()
+        {
+            var result = sut.CreateGraph(action);
+            Assert.NotNull(result);
+            Assert.IsType<GraphDbContext>(result);
+        }
+
+        [Fact()]
+        public void CreateMongoTest()
+        {
+            var result = sut.CreateMongo(action);
+            Assert.NotNull(result);
+            Assert.IsType<MongoDbContext>(result);
+        }
+
+        [Fact()]
+        public void CreateSqlTest()
+        {
+            var result = sut.CreateSql(action);
+            Assert.NotNull(result);
+            Assert.IsType<SqlDbContext>(result);
+        }
+    }
+}
---
+++ PatternTests/DbContextOptionBuilderTests.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+using FactoryPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternTests
+{
+    [Trait("Category", "Unit")]
+    public class DbContextOptionBuilderTests
+    {
+        private IDbContextOptionBuilder sut = new DbContextOptionBuilder();
+
+        [Fact()]
+        public void AddConnectionStringTest()
+        {
+            const string expected = "connString";
+            var dbOption = sut.AddConnectionString("connString").Build();
+            Assert.Equal(expected, dbOption.ConnectionString);
+        }
+
+        [Fact()]
+        public void SetTimeOutTest()
+        {
+            TimeSpan expected = TimeSpan.Zero;
+            var dbOption = sut.SetTimeOut(expected).Build();
+            Assert.Equal(expected, dbOption.Timeout);
+        }
+
+        [Fact()]
+        public void EnableMemoryCacheTest()
+        {
+            const bool expected = false;
+            var dbOption = sut.EnableMemoryCache(expected).Build();
+            Assert.Equal(expected, dbOption.EnableMemoryCache);
+        }
+
+        [Fact()]
+        public void AddDatabaseNameTest()
+        {
+            const string expected = "dbName";
+            var dbOption = sut.AddDatabaseName(expected).Build();
+            Assert.Equal(expected, dbOption.DatabaseName);
+        }
+
+        [Fact()]
+        public void BuildTest()
+        {
+            var expected = new DbContextOptions();
+            var dbOption = sut.Build();
+            Assert.Equal(expected, dbOption);
+        }
+    }
+}
